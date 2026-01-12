ac54e315b4df77c8618e57d43676880c0a2a94f8
hievt <hieuvt@bkav.com>
2026-01-12 16:56:41 +0700

    product detail, context api

--- src/app/AppProvider.tsx
+++ src/app/AppProvider.tsx
@@ -1,18 +1,44 @@
 "use client";
 
 import { clientSessionToken } from "@/lib/http";
-import { useState } from "react";
+import { AccountResType } from "@/schemaValidations/account.schema";
+import { createContext, useContext, useState } from "react";
 
+type User = AccountResType["data"];
+
+const AppContext = createContext<{
+  user: User | null;
+  setUser: (user: User | null) => void;
+}>({
+  user: null,
+  setUser: () => {},
+});
+export const userAppContetxt = () => {
+  const context = useContext(AppContext);
+  return context;
+};
 export default function AppProvider({
   children,
   initialSessionToken = "",
+  user: userProp,
 }: {
   children: React.ReactNode;
   initialSessionToken?: string;
+  user: User | null;
 }) {
+  const [user, setUser] = useState<User | null>(userProp);
   useState(() => {
     if (typeof window === "undefined") return;
     clientSessionToken.value = initialSessionToken;
   });
-  return <>{children}</>;
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import type { NextRequest } from 'next/server'
 
 const privatePaths = ['/me', '/dashboard']
 const authPaths = ['/login', '/register']
- 
+const productEditRegex = /^\/products\/\d+\/edit$/
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+
   const { pathname } = request.nextUrl;
   const sessionToken = request.cookies.get('sessionToken')?.value
   if(privatePaths.some((path) => pathname.startsWith(path)) && !sessionToken) {
@@ -14,12 +15,16 @@ export function middleware(request: NextRequest) {
   if(authPaths.some((path) => pathname.startsWith(path)) && sessionToken) {
     return NextResponse.redirect(new URL('/me', request.url))
   }
+
+  if(pathname.match(productEditRegex) && !sessionToken) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
   return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    '/me', '/dashboard','/login', '/register'
+    '/me', '/dashboard','/login', '/register', '/products/:path*'
   ],
 }
